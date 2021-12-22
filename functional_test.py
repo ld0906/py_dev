1e22604d7672ab7445d2be3846e4060e71b233c4
Id0906 <jason118@jasondeMacBook-Pro.local>
2021-12-22 22:58:07 +0800

    updates it here

--- functional_test.py
+++ functional_test.py
@@ -40,17 +40,30 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"New to-do item did not appear in table.Contents were: \n{table.text}"
+        # )
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了 "Use peacock feathers to make a fly" (使用孔雀羽毛做假蝇)
         # 小张做事很有条理。
-
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # 页面再次更新，他的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+        time.sleep(5)
+        self.fail('Finish the test!')
+
 
         # 小张想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
